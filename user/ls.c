ac475d0f3e8607adb648b97a28c35a94d5c0dc52
ekstewart <ekstewart987@gmail.com>
2021-01-18 18:59:57 -0800

    add comments to ls.c

--- user/ls.c
+++ user/ls.c
@@ -30,34 +30,45 @@ ls(char *path)
   struct dirent de;
   struct stat st;
 
+  // attain file descriptor of path
   if((fd = open(path, 0)) < 0){
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
+  // use stat to get information on that file descriptor
   if(fstat(fd, &st) < 0){
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
+  // switch statement based on file type (normal or directory)
   switch(st.type){
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
   case T_DIR:
+    // check to make sure path isnt super long
     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
       printf("ls: path too long\n");
       break;
     }
+    // copy the path onto the buf variable (so as not to modify path)
     strcpy(buf, path);
+
+    // point p to the end of the buf
     p = buf+strlen(buf);
+
+    // Add slash to show new level then increment pointer
     *p++ = '/';
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
       if(de.inum == 0)
-        continue;
+        continue; 
+      // Copy first 14(DIRSIZ) characters from de.name to p
       memmove(p, de.name, DIRSIZ);
+      // Set end of p to be null terminator
       p[DIRSIZ] = 0;
       if(stat(buf, &st) < 0){
         printf("ls: cannot stat %s\n", buf);
